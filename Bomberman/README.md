07a97356e0ac38082ae00b7a5ad1d0f38187cc7c
Carlo Pinciroli <carlo@pinciroli.net>
2019-02-06 01:40:20 -0500

    Latest testing and README

--- Bomberman/README.md
+++ Bomberman/README.md
@@ -1,3 +1,14 @@
-# Required packages #
+# Required software #
+
+To run Bomberman, you'll need Python 3 with the `colorama` package. To install it, type either
+
+    pip install colorama
+    
+if Python 3 is your default version, or
 
     pip3 install colorama
+    
+if you have both Python 2 and Python 3 installed on your system.
+
+# Running Bomberman #
+
--- Bomberman/run.py
+++ Bomberman/run.py
@@ -1,14 +1,14 @@
 from game import Game
 from entity import MonsterEntity
 from selfpreserving_monster import SelfPreservingMonster
-from character import Character
+from testcharacter import TestCharacter
 
 g = Game.fromfile('simple.txt')
 
 # g.add_monster(MonsterEntity(0,0))
-g.add_monster(SelfPreservingMonster(4,1))
+# g.add_monster(SelfPreservingMonster(4,1))
 
-c = Character("me", 0, 1)
+c = TestCharacter("me", 0, 0)
 g.add_character(c)
 # g.world.add_bomb(8,0,c)
 
