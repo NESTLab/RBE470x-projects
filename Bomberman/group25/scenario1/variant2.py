2e893885486e623374abe19d7b5c413162b313b9
Krysta <krmurdy@wpi.edu>
2019-02-11 11:43:51 -0500

    Changed groupNN to group25

--- Bomberman/group25/scenario1/variant2.py
+++ Bomberman/group25/scenario1/variant2.py
@@ -4,15 +4,21 @@ sys.path.insert(0, '../../bomberman')
 sys.path.insert(1, '..')
 
 # Import necessary stuff
+import random
 from game import Game
+from monsters.stupid_monster import StupidMonster
 
 # TODO This is your code!
-sys.path.insert(1, '../groupNN')
+sys.path.insert(1, '../group25')
 from testcharacter import TestCharacter
 
-
 # Create the game
+random.seed(123) # TODO Change this if you want different random choices
 g = Game.fromfile('map.txt')
+g.add_monster(StupidMonster("monster", # name
+                            "M",       # avatar
+                            3, 9       # position
+))
 
 # TODO Add your character
 g.add_character(TestCharacter("me", # name
