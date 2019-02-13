40a3be49495af4f84504bdaab4af84c959cd6a95
Clayton Dembski <cdembski1@gmail.com>
2019-02-13 18:01:36 -0500

    a comment boi

--- Bomberman/groupNN/testcharacter.py
+++ Bomberman/groupNN/testcharacter.py
@@ -12,9 +12,11 @@ class TestCharacter(CharacterEntity):
     def do(self, wrld):
         # Your code here
         exit = [0, 0]
+        #get current position
         meX = wrld.me(self).x
         meY = wrld.me(self).y
 
+        #check every gridcell for the exit. Uses the last exit found as the "goal"
         for i in range(wrld.width()):
 
             for j in range(wrld.height()):
@@ -22,8 +24,9 @@ class TestCharacter(CharacterEntity):
                 if wrld.exit_at(i, j):
                     exit = [i, j]
 
+        #get the [x,y] coords of the next cell to go to
         goTo = greedyBFS.getNextStep([meX, meY], exit, wrld)
 
-
+        #move in direction to get to x,y found in prev step
         self.move(-meX + goTo[0], -meY + goTo[1])
 
