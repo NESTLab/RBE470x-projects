13ebab1bcc42e5390efbcbbfadc83a3ef2c27430
Ryan Kim <rkim@wpi.edu>
2019-02-14 12:42:33 -0500

    Update testcharacter.py

--- Bomberman/groupNN/testcharacter.py
+++ Bomberman/groupNN/testcharacter.py
@@ -4,6 +4,7 @@ sys.path.insert(0, '../bomberman')
 # Import necessary stuff
 from entity import CharacterEntity
 from colorama import Fore, Back
+import PriorityQueue
 
 
 class TestCharacter(CharacterEntity):
@@ -18,12 +19,30 @@ class TestCharacter(CharacterEntity):
 
     @staticmethod
     def get_exit(wrld):
-        x1 = 0
-        y1 = 0
+        x1 = -1
+        y1 = -1
         # Find the exit to use for heuristic A*
         for x in range(0, wrld.width()):
             for y in range(0, wrld.height()):
                 if wrld.exit_at(x, y):
                     x1 = x
                     y1 = y
+        if x1 == -1:
+            return -1
         return x1, y1
+
+    # Determining the heuristic value, being Euclidean Distance
+    @staticmethod
+    def heuristic(start, goal):
+        (x1, y1) = start
+        (x2, y2) = goal
+
+        # Euclidean distance is the hypotenuse
+        # We add the squared values, and finding the sqrt is
+        # not necessary as it will never effect the outcome
+        value = (x1 + x2)**2 + (y1 + y2)**2
+        return value
+
+    @staticmethod
+    def a_star(wrld, start, goal):
+        frontier = PriorityQueue()
