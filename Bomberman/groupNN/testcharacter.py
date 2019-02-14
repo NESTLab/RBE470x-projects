e456835b603ed53e55cb382575c4480620b50cee
Ryan Kim <rkim@wpi.edu>
2019-02-13 19:33:21 -0500

    Update testcharacter.py

--- Bomberman/groupNN/testcharacter.py
+++ Bomberman/groupNN/testcharacter.py
@@ -5,8 +5,20 @@ sys.path.insert(0, '../bomberman')
 from entity import CharacterEntity
 from colorama import Fore, Back
 
+
 class TestCharacter(CharacterEntity):
 
     def do(self, wrld):
         # Your code here
+
+        x1 = 0
+        y1 = 0
+        # Find the exit to use for heuristic A*
+        for x in range(0, wrld.width()):
+            for y in range(0, wrld.height()):
+                if wrld.exit_at(x, y):
+                    x1 = x
+                    y1 = y
+        
+        print(x1, y1)
         pass
