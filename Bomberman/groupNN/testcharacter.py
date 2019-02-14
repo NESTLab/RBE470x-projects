007771a5b28d13b822098859f06386eac42a82fc
Ryan Kim <rkim@wpi.edu>
2019-02-13 22:20:54 -0500

    Update testcharacter.py

--- Bomberman/groupNN/testcharacter.py
+++ Bomberman/groupNN/testcharacter.py
@@ -8,19 +8,22 @@ from colorama import Fore, Back
 
 class TestCharacter(CharacterEntity):
 
-    def do(self, wrld):
-        # Your code here
-
-        pass
-
     @staticmethod
     def get_exit(wrld):
-        x1 = 0
-        y1 = 0
+        x1 = -1
+        y1 = -1
         # Find the exit to use for heuristic A*
         for x in range(0, wrld.width()):
             for y in range(0, wrld.height()):
                 if wrld.exit_at(x, y):
                     x1 = x
                     y1 = y
+        if x1 or y1 == -1:
+            pass
         return x1, y1
+
+    def do(self, wrld):
+        # Your code here
+        x, y = self.get_exit(wrld)
+        print(self.get_exit(wrld))
+        pass
