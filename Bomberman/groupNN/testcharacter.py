bfc15a73bf5ba63d759f8de6089f5b78e795a8dc
Andrew DeRusha <acderusha@wpi.edu>
2019-02-16 13:46:03 -0500

    printed all possible moves for agent to take

--- Bomberman/groupNN/testcharacter.py
+++ Bomberman/groupNN/testcharacter.py
@@ -4,6 +4,8 @@ sys.path.insert(0, '../bomberman')
 # Import necessary stuff
 from entity import CharacterEntity
 from colorama import Fore, Back
+# Imports for code implementation
+from queue import PriorityQueue
 
 class TestCharacter(CharacterEntity):
 
@@ -16,10 +18,10 @@ class TestCharacter(CharacterEntity):
         print(self.x, self.y)
 
         # Find the start (current position) and goal
-        start = [self.x, self.y]
+        start = (self.x, self.y)
         goal = self.findGoal(wrld)
 
-        a_star_path = self.a_star_search(wrld, start,goal)
+        a_star_path = self.a_star_search(wrld, start, goal)
 
         pass
 
@@ -36,11 +38,11 @@ class TestCharacter(CharacterEntity):
             for x in range(wrld.width()):
                 if(wrld.exit_at(x,y)):
                     print(x,y)
-                    return [x,y]
+                    return (x,y)
 
         # Return impossible exit coordinate to signal no exit found
         print('No Exit Found')
-        return [-1,-1]
+        return (-1,-1)
 
     # Returns a world with the A* path marked
     #
@@ -49,4 +51,61 @@ class TestCharacter(CharacterEntity):
     #                                       [goal.x, goal.y]: the x and y coordinated of the goal / exit
     # RETURNS: [world] newWrld: a grid world marking the A* path to the goal
     #
+    def a_star_search(self, wrld, start, goal):
 
+        print(self.getAllMoves(wrld, start))
+        # frontier = PriorityQueue()
+        # frontier.put(start, 0)
+        # came_from = {}
+        # cost_so_far = {}
+        # came_from[start] = None
+        # cost_so_far[start] = 0
+        #
+        # while not frontier.empty():
+        #     current = frontier.get()
+        #
+        #     if current == goal:
+        #         break
+        #
+        #     for next in self.getAllMoves(wrld, current):
+        #         new_cost = cost_so_far[current] + wrld.cost(current, next)
+        #         if next not in cost_so_far or new_cost < cost_so_far[next]:
+        #             cost_so_far[next] = new_cost
+        #             priority = new_cost
+        #             frontier.put(next, priority)
+        #             came_from[next] = current
+        #
+        # return came_from, cost_so_far
+
+
+    # Returns list of all the possible moves for agent (up, down, left, right)
+    #
+    # PARAM: [world, [int, int]] wrld: the current world configuration
+    #        [start.x, start.y] currentLocation: the x and y coordinated the agent is located at
+    # RETURNS: [list [int, int]] allMoves: a list of all the possible moves for agent
+    #
+    def getAllMoves(self, wrld, currentLocation):
+        movesList = []
+
+        # Look right
+        # Avoid out of bound look ups
+        if((currentLocation[0] + 1) < wrld.width()):
+            if(wrld.empty_at(currentLocation[0] + 1, currentLocation[1])):
+                movesList.append((currentLocation[0] + 1, currentLocation[1]))
+        # Look left
+        # Avoid out of bound look ups
+        if ((currentLocation[0] - 1) >= 0):
+            if (wrld.empty_at(currentLocation[0] - 1, currentLocation[1])):
+                movesList.append((currentLocation[0] - 1, currentLocation[1]))
+        # Look down
+        # Avoid out of bound look ups
+        if ((currentLocation[1] - 1) >= 0):
+            if (wrld.empty_at(currentLocation[0], currentLocation[1] - 1)):
+                movesList.append((currentLocation[0], currentLocation[1] - 1))
+        # Look up
+        # Avoid out of bound look ups
+        if ((currentLocation[1] + 1) < wrld.height()):
+            if (wrld.empty_at(currentLocation[0], currentLocation[1] + 1)):
+                movesList.append((currentLocation[0], currentLocation[1] + 1))
+
+        return movesList
