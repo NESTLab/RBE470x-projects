19e07ef46375309cb62bfc1c5944ed4c3128db56
krbrez <38116068+krbrez@users.noreply.github.com>
2019-02-19 00:23:59 -0500

    Code ifs, write helper skeletons.  At this point, all lead to greedy.

--- Bomberman/groupNN/finitestatecharacter.py
+++ Bomberman/groupNN/finitestatecharacter.py
@@ -11,18 +11,67 @@ class FiniteStateCharacter(CharacterEntity):
     def do(self, wrld):
         # This method calls different algorithms to find the next position to
         # move based on the finite state the character is in.
-        if()
-        # Character is alone and able to push forward unimpeded by wall
-        elif()
-        # Character is alone but there is a wall impeding movement
-        elif()
-        # There is at least 1 bomb within 2 steps
-        elif()
-        # There is at least 1 monster within 2 steps
-        elif()
-        # There are 2 monsters within 2 steps
-        elif()
-        # There is both at least 1 bomb and 1 monster within 2 steps
-        else()
-        # there should be nothing left, if anything gets to this point, fix it
-        pass
+
+        # A list of objects within the perimeter of 2 spaces
+        closeObjects = self.checkPerimeter2(wrld)
+        # True if there is an impassable wall within 1 space,
+        # False otherwise
+        isThereWall = self.impassableWall(wrld)
+        # True if there is at least 1 bomb within 2 steps
+        isThereBomb = isThereBomb(closeObjects)
+        # True if there is at least 1 monster within 2 steps
+        isThereMonster = isThereMonster(closeObjects)
+
+        if not closeObjects and not isThereWall:
+            # Character is alone and able to push forward unimpeded by wall
+            self.greedy(wrld)
+        elif not closeObjects and isThereWall:
+            # Character is alone but there is a wall impeding movement
+            self.greedy(wlrd)
+        elif isThereBomb and isThereMonster:
+            # There is both at least 1 bomb and 1 monster within 2 steps
+            self.greedy(wrld)
+        elif isThereMonster:
+            # There is at least 1 monster within 2 steps
+            self.greedy(wrld)
+        elif isThereBomb:
+            # There is at least 1 bomb within 2 steps
+            self.greedy(wrld)
+        else:
+            # there should be nothing left, if anything gets to this point,
+            # fix it
+            self.greedy(wrld)
+
+    def checkPerimeter2(self, wrld):
+        # Check the perimeter around the character at a depth of 2 for any other
+        # objects, return a list of the objects within the perimeter
+
+        # Note: we could also have it return a list of tuples [object, type, position]
+        # if we think the if elses will operate better that way
+        closeObjects = {}
+        return closeObjects
+
+    def impassableWall(self, wrld):
+        # Check to see if there is an impassable wall a step ahead of the
+        # character
+        isThereWall = False
+
+        # Note: can be changed to be closer or farther away from wall
+        return isThereWall
+
+    def isThereBomb(closeObjects):
+        # Use the established list of objects within the perimeter to see if
+        # there is a bomb nearby
+        isThereBomb = False
+
+        return isThereBomb
+
+    def isThereWall(closeObjects):
+        # Use the established list of objects within the perimeter to see if
+        # there is a wall nearby
+        isThereWall = False
+
+        return isThereWall
+
+    def greedy(self, wrld):
+        # Complete the greedy algorithm
