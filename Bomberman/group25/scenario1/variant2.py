77ff78edd6cff3a133e738f49618c1036d0b114a
Jeffrey Xiao <jyxiao@wpi.edu>
2019-02-21 00:51:16 -0500

    added heuristic to scenario 1, variant 2

--- Bomberman/group25/scenario1/variant2.py
+++ Bomberman/group25/scenario1/variant2.py
@@ -10,7 +10,7 @@ from monsters.stupid_monster import StupidMonster
 
 # TODO This is your code!
 sys.path.insert(1, '../group25')
-from aStarCharacter import TestCharacter
+from aStarCharacterWithHeuristic import TestCharacter
 
 # Create the game
 random.seed(1123) # TODO Change this if you want different random choices
