c59e5b84cd6ef98fa46c12fe928bc3d979bb0bae
Carlo Pinciroli <carlo@pinciroli.net>
2019-02-25 19:49:55 -0500

    Changed names of monsters so they can be identified in code

--- Bomberman/groupNN/scenario2/variant3.py
+++ Bomberman/groupNN/scenario2/variant3.py
@@ -15,10 +15,10 @@ from testcharacter import TestCharacter
 # Create the game
 random.seed(123) # TODO Change this if you want different random choices
 g = Game.fromfile('map.txt')
-g.add_monster(SelfPreservingMonster("monster", # name
-                                    "M",       # avatar
-                                    3, 9,      # position
-                                    1          # detection range
+g.add_monster(SelfPreservingMonster("selfpreserving", # name
+                                    "S",              # avatar
+                                    3, 9,             # position
+                                    1                 # detection range
 ))
 
 # TODO Add your character
--- Bomberman/groupNN/scenario2/variant5.py
+++ Bomberman/groupNN/scenario2/variant5.py
@@ -16,14 +16,14 @@ from testcharacter import TestCharacter
 # Create the game
 random.seed(123) # TODO Change this if you want different random choices
 g = Game.fromfile('map.txt')
-g.add_monster(StupidMonster("monster", # name
-                            "S",       # avatar
-                            3, 5,      # position
+g.add_monster(StupidMonster("stupid", # name
+                            "S",      # avatar
+                            3, 5,     # position
 ))
-g.add_monster(SelfPreservingMonster("monster", # name
-                                    "A",       # avatar
-                                    3, 13,     # position
-                                    2          # detection range
+g.add_monster(SelfPreservingMonster("aggressive", # name
+                                    "A",          # avatar
+                                    3, 13,        # position
+                                    2             # detection range
 ))
 
 # TODO Add your character
