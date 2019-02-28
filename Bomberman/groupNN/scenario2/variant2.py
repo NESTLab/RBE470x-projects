14649fe1987e2bc1c9c8419a2bf9c9696b49d936
James Corse <jacorse@wpi.edu>
2019-02-28 16:57:14 -0500

    variant

--- Bomberman/groupNN/scenario2/variant2.py
+++ Bomberman/groupNN/scenario2/variant2.py
@@ -10,10 +10,10 @@ from monsters.stupid_monster import StupidMonster
 
 # TODO This is your code!
 sys.path.insert(1, '../groupNN')
-from testcharacter import TestCharacter
+from testcharacter2_3 import TestCharacter
 
 # Create the game
-random.seed(123) # TODO Change this if you want different random choices
+random.seed(120) # TODO Change this if you want different random choices
 g = Game.fromfile('map.txt')
 g.add_monster(StupidMonster("monster", # name
                             "M",       # avatar
