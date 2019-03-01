ef921308702527081ec78af84e230db080039391
hdunphy <hdunphy15@gmail.com>
2019-02-28 23:57:34 -0500

    7/10 using bombs

--- Bomberman/group22/scenario1/variant4.py
+++ Bomberman/group22/scenario1/variant4.py
@@ -10,11 +10,13 @@ from monsters.selfpreserving_monster import SelfPreservingMonster
 
 # TODO This is your code!
 sys.path.insert(1, '../groupNN')
-from minMax import MinMax
+# from minMax import MinMax
+from testcharacter4 import TestCharacter
 
 # Create the game
-random.seed(3)
-# failed 2 3 5 7 9
+random.seed(9)
+# failed 4 7 8
+# passed 1 2 3 5 6
 g = Game.fromfile('map.txt')
 g.add_monster(SelfPreservingMonster("aggressive", # name
                                     "A",          # avatar
@@ -23,7 +25,7 @@ g.add_monster(SelfPreservingMonster("aggressive", # name
 ))
 
 # TODO Add your character
-g.add_character(MinMax("me", # name
+g.add_character(TestCharacter("me", # name
                               "C",  # avatar
                               0, 0  # position
 ))
--- Bomberman/group22/testcharacter4.py
+++ Bomberman/group22/testcharacter4.py
@@ -21,6 +21,9 @@ class TestCharacter(CharacterEntity):
         self.state = NORMAL
         self.prev_state = None
         self.monster_dist = 0
+        self.last_mon = None
+        self.bomb = None
+        self.explosions = []
 
     def do(self, wrld):
         if self.exit_position is None:
@@ -33,17 +36,32 @@ class TestCharacter(CharacterEntity):
                     break
 
         monster = None
+        self.explosions = []
+        self.bomb = None
+        # explosion = None
+        # bomb = None
         for x in range(0, wrld.width()):
             for y in range(0, wrld.height()):
                 if wrld.monsters_at(x, y):
                     monster = (x, y)
-                if monster is not None:
-                    break
+                explosion = wrld.explosion_at(x, y)
+                if explosion is not None:
+                    self.explosions.append(explosion)
+                bomb = wrld.bomb_at(x, y)
+                if bomb is not None:
+                    self.bomb = bomb
 
         self.prev_state = self.state
-        self.monster_dist = self.get_distance_between((self.x, self.y), (monster[0], monster[1]))
-        if self.monster_dist < 4:
-
+        if monster is not None:
+            self.monster_dist = self.get_distance_between((self.x, self.y), (monster[0], monster[1]))
+        else:
+            self.monster_dist = 100
+        if self.in_corner(wrld) and 9 > self.monster_dist > 3 and self.bomb is None and len(self.explosions) == 0:
+            self.state = BOMB
+            self.place_bomb()
+        elif len(self.explosions) > 0 or self.bomb is not None:
+            self.state = BOMB
+        elif self.monster_dist < 6:
             self.state = MONSTER
         else:
             self.state = NORMAL
@@ -60,15 +78,33 @@ class TestCharacter(CharacterEntity):
         elif self.state == MONSTER:
             next_move = self.avoid_monster(wrld, monster)
             # self.a_star(frontier, cost_so_far, wrld, monster)
+        elif self.state == BOMB:
+            next_move = self.avoid_bomb(wrld, monster)
 
         self.move(next_move[0] - self.x, next_move[1] - self.y)
+        self.last_mon = monster
 
     def avoid_monster(self, wrld, monster):
         next_move = None
+        if monster is None:
+            monster = (0, 0)
         possible_moves = self.get_possible_moves(self.x, self.y, wrld)
         value = -10000000
         for move in possible_moves:
             expected = self.exp_value(wrld, self.exit_position, move[0], move[1], monster[0], monster[1], 0)
+            if expected >= value:
+                value = expected
+                next_move = move
+        return next_move
+
+    def avoid_bomb(self, wrld, monster):
+        next_move = None
+        if monster is None:
+            monster = (0, 0)
+        possible_moves = self.get_possible_moves(self.x, self.y, wrld)
+        value = -10000000
+        for move in possible_moves:
+            expected = self.exp_value_bomb(wrld, self.exit_position, move[0], move[1], monster[0], monster[1], 0)
             if expected >= value:
                 value = expected
                 next_move = move
@@ -93,11 +129,12 @@ class TestCharacter(CharacterEntity):
         monst_distance = self.get_distance_between((char_x, char_y), (monster_x, monster_y))
         if (char_x, char_y) == exit_position:
             return (0.9 ** depth) * 100000
+        if (char_x, char_y) == (monster_x, monster_y):
+            return (0.9 ** depth) * -10000
+        elif monst_distance < 3:
+            return (0.9 ** depth) * -5000
         value = 0
-        if monst_distance < 2:
-            monster_moves = self.get_monster_moves(char_x, char_y, monster_x, monster_y, wrld)
-        else:
-            monster_moves = self.get_possible_moves(monster_x, monster_y, wrld)
+        monster_moves = self.get_possible_moves(monster_x, monster_y, wrld)
         for move in monster_moves:
             prob = 1 / len(monster_moves)
             v = self.max_value(wrld, exit_position, char_x, char_y, move[0], move[1], depth)
@@ -108,17 +145,65 @@ class TestCharacter(CharacterEntity):
         monst_distance = self.get_distance_between((char_x, char_y), (monster_x, monster_y))
         if (char_x, char_y) == (monster_x, monster_y):
             return (0.9 ** depth) * -10000
-        elif monst_distance == 1:
+        elif monst_distance < 3:
             return (0.9 ** depth) * -5000
-        elif depth > 0:
+        elif depth > 1:
             exit_dist = self.get_distance_between((char_x, char_y), exit_position)
-            return (0.9 ** depth) * ((monst_distance / 2) + (34 / exit_dist))  # ((monst_distance) + (1 / exit_dist))
+            return (0.9 ** depth) * ((monst_distance) + (75 / (1 + exit_dist)))  # ((monst_distance) + (1 / exit_dist))
         value = -1000000000
         character_moves = self.get_possible_moves(char_x, char_y, wrld)
         for move in character_moves:
             v = self.exp_value(wrld, exit_position, move[0], move[1], monster_x, monster_y, depth + 1)
             value = max(value, v)
         return value
+
+    def exp_value_bomb(self, wrld, exit_position, char_x, char_y, monster_x, monster_y, depth):
+        terminal = self.terminal_state(char_x, char_y, monster_x, monster_y, depth)
+        if terminal is not None:
+            return terminal
+        value = 0
+        monster_moves = self.get_possible_moves(monster_x, monster_y, wrld)
+        for move in monster_moves:
+            prob = 1 / len(monster_moves)
+            v = self.max_value_bomb(wrld, exit_position, char_x, char_y, move[0], move[1], depth)
+            value = value + prob * v
+        return value
+
+    def max_value_bomb(self, wrld, exit_position, char_x, char_y, monster_x, monster_y, depth):
+        terminal = self.terminal_state(char_x, char_y, monster_x, monster_y, depth)
+        if terminal is not None:
+            return terminal
+        value = -1000000000
+        character_moves = self.get_possible_moves(char_x, char_y, wrld)
+        for move in character_moves:
+            v = self.exp_value_bomb(wrld, exit_position, move[0], move[1], monster_x, monster_y, depth + 1)
+            value = max(value, v)
+        return value
+
+    def terminal_state(self, char_x, char_y, monster_x, monster_y, depth):
+        monst_distance = self.get_distance_between((char_x, char_y), (monster_x, monster_y))
+        bad = (0.9 ** depth) * -100
+        if (char_x, char_y) == (monster_x, monster_y):
+            return bad
+        if self.bomb is not None and self.bomb.timer - depth <= 1:
+            if char_x == self.bomb.x or char_y == self.bomb.y:
+                return bad
+        if len(self.explosions) > 0:
+            for explo in self.explosions:
+                explo_dist = self.get_distance_between((char_x, char_y), (explo.x, explo.y))
+                if explo_dist == 0:
+                    return bad
+        if (char_x, char_y) == self.exit_position:
+            return (0.9 ** depth) * 100000
+        if monst_distance < 3:
+            return bad
+        if depth > 1:
+            exit_dist = self.get_distance_between((char_x, char_y), self.exit_position)
+            bomb_dist = 0
+            if self.bomb is not None:
+                bomb_dist = self.get_distance_between((char_x, char_y), (self.bomb.x, self.bomb.y)) / 3
+            return (0.9 ** depth) * (monst_distance + (100 / (1 + exit_dist)) + bomb_dist)
+        return None
 
     def find_next_move(self, came_from, exit_position):
         next_move = exit_position
@@ -146,21 +231,33 @@ class TestCharacter(CharacterEntity):
                                 array.append((x + dx, y + dy))
         return array
 
-    def get_monster_moves(self, char_x, char_y, x, y, wrld):
+    def get_monster_moves(self, x, y, wrld):
         array = []
-        for dx in [-1, 0, 1]:
-            # Avoid out-of-bound indexing
-            if (x + dx >= 0) and (x + dx < wrld.width()):
-                # Loop through delta y
-                for dy in [-1, 0, 1]:
-                    # Make sure the monster is moving
-                    if (dx != 0) or (dy != 0):
+        if self.last_mon is None:
+            m_dx = 0
+            m_dy = 0
+        else:
+            m_dx = x - self.last_mon[0]
+            m_dy = y - self.last_mon[1]
+
+        out_of_bounds = False
+        if (x + m_dx >= 0) or (x + m_dx < wrld.width()) or (y + m_dy >= 0) or (y + m_dy < wrld.height()):
+            out_of_bounds = True
+        if not out_of_bounds and not wrld.wall_at(x + m_dx, y + m_dy):
+            array.append((x + m_dx, y + m_dy))
+            return array
+        else:
+            for dx in [-1, 0, 1]:
+                # Avoid out-of-bound indexing
+                if (x + dx >= 0) and (x + dx < wrld.width()):
+                    # Loop through delta y
+                    for dy in [-1, 0, 1]:
+                        # Make sure the monster is moving
+                        if (dx != 0) or (dy != 0):
                         # Avoid out-of-bound indexing
-                        if (y + dy >= 0) and (y + dy < wrld.height()):
-                            # No need to check impossible moves
-                            if not wrld.wall_at(x + dx, y + dy):
-                                distance = self.get_distance_between((char_x, char_y), (x, y))
-                                if distance < 2:
+                            if (y + dy >= 0) and (y + dy < wrld.height()):
+                                # No need to check impossible moves
+                                if not wrld.wall_at(x + dx, y + dy):
                                     array.append((x + dx, y + dy))
         return array
 
@@ -168,6 +265,24 @@ class TestCharacter(CharacterEntity):
         # return math.sqrt((position[1] - position2[1]) * (position[1] - position2[1]) +
         #                  (position[0] - position2[0]) * (position[0] - position2[0]))
         return max(abs(position[0] - position2[0]), abs(position[1] - position2[1]))
+
+    def in_corner(self, wrld):
+        x = self.x
+        y = self.y
+        for dx in [-1, 0, 1]:
+            # Avoid out-of-bound indexing
+            if (x + dx >= 0) and (x + dx < wrld.width()):
+                # Loop through delta y
+                for dy in [-1, 0, 1]:
+                    # Make sure the monster is moving
+                    if (dx == 0) or (dy == 0):
+                        # Avoid out-of-bound indexing
+                        if (y + dy >= 0) and (y + dy < wrld.height()):
+                            # No need to check impossible moves
+                            if wrld.wall_at(x + dx, y + dy):
+                                return True
+        return False
+
 
 
 class PQueue:
