7e0b4b25c82ef7af61296de85ed5da0ada132e6b
Carlo Pinciroli <carlo@pinciroli.net>
2020-03-07 16:50:02 -0500

    Added logged game in ConnectN

--- ConnectN/game.py
+++ ConnectN/game.py
@@ -1,6 +1,7 @@
 import board
 import agent
 import time
+from pathlib import Path
 
 ########
 # Game #
@@ -66,6 +67,7 @@ class Game(object):
     # In a timed game, if a player takes more than 'limit' seconds to make a
     # move, it loses.
     #
+    # PARAM  [int] limit: the time limit in seconds
     # RETURN [int]: The game outcome.
     #               1 for Player 1, 2 for Player 2, and 0 for no winner
     def timed_go(self, limit):
@@ -91,5 +93,51 @@ class Game(object):
                 p = 1
             else:
                 p = 0
+        # Return game outcome
+        return self.board.get_outcome()
+
+    # Execute a timed game.
+    #
+    # In a timed game, if a player takes more than 'limit' seconds to make a
+    # move, it loses.
+    #
+    # PARAM  [int] log_file: the path of the log file
+    # PARAM  [int] limit: the time limit in seconds
+    # RETURN [int]: The game outcome.
+    #               1 for Player 1, 2 for Player 2, and 0 for no winner
+    def logged_go(self, log_file, limit):
+        with Path(log_file).open("w") as log:
+            log.write("{} player1\n".format(self.players[0].name))
+            log.write("{} player2\n".format(self.players[1].name))
+            # Current player
+            p = 0
+            while self.board.free_cols() and self.board.get_outcome() == 0:
+                # Get start time
+                st = time.time()
+                # Make move and copy board so player can't modify it
+                x = self.players[p].go(self.board.copy())
+                # Get elapsed time
+                et = time.time() - st
+                # Is the move legal and within the time limit?
+                if (not x in self.board.free_cols()) or (et > limit):
+                    # Illegal/out of time, nothing to log, end of game
+                    outcome = 1
+                    if p == 0:
+                        outcome = 2
+                    return outcome
+                # Legal move, add token there
+                self.board.add_token(x)
+                # Log move
+                log.write("{} {}\n".format(self.players[p].name, x))
+                # Switch player
+                if p == 0:
+                    p = 1
+                else:
+                    p = 0
+            # Game ended successfully, log the outcome
+            if self.board.get_outcome() == 0:
+                log.write("- tie\n".format(self.players[0].name))
+            else:
+                log.write("{} wins\n".format(self.players[self.board.get_outcome()-1].name))
         # Return game outcome
         return self.board.get_outcome()
