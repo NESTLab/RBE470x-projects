f3223ec1fa768dfd53b1b8aa8f37bf83c37dfe6a
tyler <tylerferrara123@gmail.com>
2021-02-16 12:52:25 -0500

    added training.py & fixing num_in_a_row errors

---
+++ ConnectN/README.md
@@ -0,0 +1,33 @@
+##TODO
+
+- create a new minmax function that adds alpha beta pruning (don't delete the old one)
+- count number of n-in-a-row diagnals (found in num_in_a_row)
+- modify training.py to print out a COMPLETE analysis
+    tokens_to_win = 3
+    depth = 4
+    H X W   Win/Loss
+    6 X 7       0.3
+    7 X 9       0.5
+    1 X 2       0.9
+    5 X 5       0.92
+
+    tokens_to_win = 4
+    depth = 4
+    Depth   Win/Loss
+    H X W   Win/Loss
+    6 X 7       0.3
+    7 X 9       0.5
+    1 X 2       0.9
+    5 X 5       0.92
+
+    tokens_to_win = 5
+    depth = 4
+    Depth   Win/Loss
+    H X W   Win/Loss
+    6 X 7       0.3
+    7 X 9       0.5
+    1 X 2       0.9
+    5 X 5       0.92
+
+
+- modify training.py to log to file every board where the AI Lost (Include all params, h, w, time_limit, tokens_to_win)
--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -11,11 +11,17 @@ class AlphaBetaAgent(agent.Agent):
     # Class constructor.
     #
     # PARAM [string] name:      the name of this player
-    # PARAM [int]    max_depth: the maximum search depth
-    def __init__(self, name, max_depth):
+    # PARAM [int] max_depth: the maximum search depth
+    # PARAM [int] to_win: number of pieces in a row to win
+    # PARAM [bool] isPlayer1: true if the AI is player 1
+    def __init__(self, name, max_depth, to_win):
         super().__init__(name)
         # Max search depth
         self.max_depth = max_depth
+        # Num of pieces in a row to win
+        self.to_win = to_win
+        self.player = 0
+
 
     # Pick a column.
     #
@@ -25,7 +31,14 @@ class AlphaBetaAgent(agent.Agent):
     # NOTE: make sure the column is legal, or you'll lose the game.
     def go(self, brd):
         """Search for the best move (choice of column for the token)"""
-        # Your code here
+
+
+        # find opponent token
+        if self.player == 0:
+            self.player = self.findPlayer(brd)
+
+
+        return self.find_best_column(brd)
 
     # Get the successors of the given board.
     #
@@ -51,3 +64,110 @@ class AlphaBetaAgent(agent.Agent):
             # Add board to list of successors
             succ.append((nb,col))
         return succ
+
+    # use the minimax algorithm to choose the best column to add to
+    #
+    # PARAM  [board.Board] brd: the current board state
+    # RETURN [int]: index of the column to add a the next move
+    #
+    def find_best_column(self, brd):
+        val = float('-inf')
+        move = -1
+        for child in self.get_successors(brd):
+            next_brd = child[0]
+            next_move = child[1]
+            found_val = self.minimax(next_brd, self.max_depth, True)
+            if found_val > val:
+                val = found_val
+                move = next_move
+        return move
+
+    # minimax algorithm to be called by go
+    #
+    # PARAM  [board.Board] brd: the board state
+    # PARAM  [int] depth: the max depth of recursive calls
+    # PARAM  [bool] max_node: determins if max or min node
+    # RETURN [float]: value of given decision tree
+    #
+    def minimax(self, brd, depth, max_node):
+        # is the game over?
+        if depth == 0 or len(brd.free_cols()) == 0 or brd.get_outcome() != 0:
+            
+            res = self.evaluate(brd)
+            # print(brd.print_it())
+            # print("above evaluation: ", res)
+            # print("depth ", depth, " free_cols ", len(brd.free_cols()), " outcome ", brd.get_outcome())
+            return res
+        # max
+        if max_node:
+            v = float('-inf')
+            for child in self.get_successors(brd):
+                res = self.minimax(child[0], depth-1, False)
+                v = max(v, res)
+            return v
+        # min
+        v = float('inf')
+        for child in self.get_successors(brd):
+            res = self.minimax(child[0], depth-1, True)
+            v = min(v, res)
+        return v
+
+    def evaluate(self, brd):
+        return self.num_in_a_row(brd) * self.win_weight(brd)
+    
+    def win_weight(self, brd):
+        if self.player == brd.get_outcome():
+            return 200
+        elif brd.get_outcome() != 0:
+            return -200
+        else:
+            return 1
+
+    def num_in_a_row(self, brd):
+        # each points[i] is number of occurances with i+1 in a row
+        points = []
+        for _ in range(self.to_win):
+            points.append(0)
+        h = brd.h
+        w = brd.w
+        # horizontal
+        seen = 0
+        for row in brd.board:
+            for col in row:
+                if col == self.player:
+                    seen = seen + 1
+                elif seen > 0 and seen-1 < len(points):
+                    points[seen-1] = points[seen-1] + 1
+                    seen = 0
+        # vertical
+        seen = 0
+        for col in range(w):
+            for row in range(h):
+                if brd.board[row][col] == self.player:
+                    seen = seen + 1
+                elif seen > 0 and seen-1 < len(points):
+                    points[seen-1] = points[seen-1] + 1
+                    seen = 0
+        
+        # TODO *********** horizontal
+
+        # count points
+        result = 0
+        for v in points:
+            result = result + self.quad_scalar(v)
+        return result
+
+    def quad_scalar(self, x):
+        return x*x*x/self.to_win
+        
+    
+    # run once at the first move of the agent, finding which piece to place
+    # [PARAM] brd: board from game
+    # [int]: which player the AI is playing as
+    #
+    def findPlayer(self, brd):
+        for row in brd.board:
+            for col in row:
+                if col != 0:
+                    return 2
+        return 1
